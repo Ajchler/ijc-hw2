1cc305fc3321a11b7d7ed1deb3b0eb9d9276a3f8
Vojtěch Eichler <vojta.eich@gmail.com>
2021-04-04 21:08:23 +0200

    Added htab_clear implementation and fixed bugs

--- htab_init.c
+++ htab_init.c
@@ -5,20 +5,21 @@
 // Datum: 4. 4. 2021
 // Popis: Funkce na alokaci a inizializaci hash table
 
-#include <htab.h>
-#include <htab_def.h>
+#include <stdlib.h>
+#include "htab_def.h"
+#include "htab.h"
 
-htab_t *htab_init(size_t number) {
+htab_t *htab_init(size_t n) {
 	htab_t *table;
 	// if allocation fails, return NULL, allocating space for hash table + 
 	// n * pointer to hash table item/record
-	if (!(table = malloc(sizeof(htab_t) + number * sizeof(htab_item_t*))))
+	if (!(table = malloc(sizeof(htab_t) + n * sizeof(htab_item_t*))))
 		return NULL;
 
 	// hash table initialization
 	table->size = 0;
-	table->arr_size = number;
-	for (size_t i = 0; i < number; i++)
+	table->arr_size = n;
+	for (size_t i = 0; i < n; i++)
 		table->items[i] = NULL;
 
 	return table;
