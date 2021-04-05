b3fa346ab68c4345d46805b59f45f78a114d9da4
Vojtěch Eichler <vojta.eich@gmail.com>
2021-04-05 15:56:02 +0200

    Implemented htab_find, htab_lookup_add, htab_free and htab_clear

---
+++ htab_lookup_add.c
@@ -0,0 +1,16 @@
+// htab_lookup_add.c
+// Řešení IJC-DU2, druhy ukol
+// Autor: Vojtěch Eichler (xiechl01)
+// Fakulta informačních technologií VUT Brno
+// Datum: 5. 4. 2021
+// Popis: Funkce, která vyhledá záznam podle key, pokud najde, vrátí ukazatel na
+// 	pair, pokud nenajde, vyvtvoří nový záznam a vrátí ukazatel na něj. Při
+// 	selhání alokace, vrací NULL.
+
+#include <string.h>
+#include "htab.h"
+#include "htab_def.h"
+
+htab_pair_t *htab_lookup_add(htab_t *t, htab_key_t key) {
+
+}
